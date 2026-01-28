59ab5048cef8240c16e09b8b10418c5922aa8078
Oliver Cecil <ocecil98@gmail.com>
2026-01-28 16:57:47 -0500

    Added port specifying

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,47 @@
 #include "ansi.h"
 #include "logger.h"
 
+int get_port(int argc, char* argv[]);
 
-int main()
+int main(const int argc, char* argv[])
 {
     Ansi::enable_ansi();
 
-    Logger::log_info("This is an info message");
-    Logger::log_warning("This is a warning message");
-    Logger::log_error("This is an error message");
-    Logger::log_success("This is a success message");
+    const int port = get_port(argc, argv);
+    Logger::log_info("Starting server on port " + std::to_string(port));
 
     return 0;
+}
+
+int get_port(const int argc, char* argv[])
+{
+    int port = 8000;
+    for (int i = 1; i < argc; i++)
+    {
+        std::string arg = argv[i];
+
+        if (arg == "--port")
+        {
+            if (i + 1 < argc)
+            {
+                try
+                {
+                    port = std::stoi(argv[i + 1]);
+                    i++; // skip next argument since we consumed it
+                }
+                catch (...)
+                {
+                    std::cerr << "Invalid value for --port\n";
+                    return 1;
+                }
+            }
+            else
+            {
+                std::cerr << "--port requires a number\n";
+                return 1;
+            }
+        }
+    }
+
+    return port;
 }
